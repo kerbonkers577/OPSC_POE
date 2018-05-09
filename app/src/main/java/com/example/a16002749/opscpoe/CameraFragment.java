ccd0e37414d0b7ca8c0329825eeebbdab5d394c9
Tristan Constable <tristan.m.constable@gmail.com>
2018-05-09 17:04:16 +0200

    fixed camera fragment showing up in values

--- app/src/main/java/com/example/a16002749/opscpoe/CameraFragment.java
+++ app/src/main/java/com/example/a16002749/opscpoe/CameraFragment.java
@@ -15,6 +15,6 @@ public class CameraFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.input_fragment, container, false);
+        return inflater.inflate(R.layout.camera_fragment, container, false);
     }
 }
